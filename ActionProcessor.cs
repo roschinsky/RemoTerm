df583ab6d855e9fac9de172e3dd4310794ae884f
Thomas Roschinsky <t.roschinsky@gmx.de>
2026-06-13 15:29:09 +0200

    extended Execute action to handle parameters properly

--- ActionProcessor.cs
+++ ActionProcessor.cs
@@ -106,7 +106,25 @@ public class ActionProcessor
     {
         try
         {
-            Process.Start(config.Payload);
+            if(string.IsNullOrEmpty(config.Payload))
+            {
+                Log.Add(new JournalEntry("No command provided to execute."));
+                return;
+            }
+
+            Process? proc;
+            string[] parts = config.Payload.Split('|');
+            if (parts.Length > 1)
+            {
+                Log.Add(new JournalEntry("Running command with arguments."));
+                proc = Process.Start(parts[0], parts[1]);
+            }
+            else
+            {
+                proc = Process.Start(config.Payload);
+            }
+
+            Log.Add(new JournalEntry($"Launched process {proc?.ProcessName ?? "Unknown"} with PID {proc?.Id ?? 0}."));
         }
         catch (Exception ex)
         {
